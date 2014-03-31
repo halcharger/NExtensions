f71b54a652742cc2c3749ac94c387c35f4c2cdc7
halcharger <allen.firth@gmail.com>
2014-03-31 16:57:56 +0100

    adding tests for StringExtensions.IsNullOrEmpty

---
+++ Tests/StringExtensionTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using NExtensions;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class StringExtensionTests
+    {
+        [Test]
+        public void IsNullOrEmpty_ShouldReturn_True_WhenStringIsNull()
+        {
+            string value = null;
+
+            value.IsNullOrEmpty().Should().BeTrue();
+        }
+
+        [Test]
+        public void IsNullOrEmpty_ShouldReturn_True_WhenStringIsEmpty()
+        {
+            string value = "";
+
+            value.IsNullOrEmpty().Should().BeTrue();
+        }
+
+        [Test]
+        public void IsNullOrEmpty_ShouldReturn_False_WhenStringHasValue()
+        {
+            string value = "boom";
+
+            value.IsNullOrEmpty().Should().BeFalse();
+        }
+    }
+}
