3ce4c7daae43a2778f59f978ad4595ee6a7d33b4
halcharger <allen.firth@gmail.com>
2014-03-31 14:34:20 +0100

    adding nuget package spec file and pkg file

--- NExtensions/Properties/AssemblyInfo.cs
+++ NExtensions/Properties/AssemblyInfo.cs
@@ -1,14 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NExtensions")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Adding .NET framework extension methods to basic types that you thought would have been there but aren't")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("halcharger")]
 [assembly: AssemblyProduct("NExtensions")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
