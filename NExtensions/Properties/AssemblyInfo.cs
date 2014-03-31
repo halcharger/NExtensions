cf77914cd4e70f234e2b89e251ade2b1e2febb75
halcharger <allen.firth@gmail.com>
2014-03-31 15:01:12 +0100

    minor changes to nuget spec file and adding nuget package icon

--- NExtensions/Properties/AssemblyInfo.cs
+++ NExtensions/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NExtensions")]
-[assembly: AssemblyDescription("Adding .NET framework extension methods to basic types that you thought would have been there but aren't")]
+[assembly: AssemblyDescription("Adding .NET framework extension methods to basic types")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("halcharger")]
 [assembly: AssemblyProduct("NExtensions")]
