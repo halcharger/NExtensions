390e8f890760db11e768878dadbb6f3dd5ec885b
halcharger <allen.firth@gmail.com>
2014-03-31 15:19:18 +0100

    adding enumerbale extensions for ForEach and adding xml comments to string extensions

---
+++ NExtensions/EnumerableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NExtensions
+{
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Executes the given Action<T> for each item in the enumerable
+        /// </summary>
+        public static void Foreach<T>(this IEnumerable<T> enumerable, Action<T> action)
+        {
+            foreach (var item in enumerable) action(item);
+        }
+    }
+}
