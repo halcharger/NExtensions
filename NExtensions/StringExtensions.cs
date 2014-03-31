eac919bec201936a103bebbdaf967c8704a80590
halcharger <allen.firth@gmail.com>
2014-03-31 14:18:00 +0100

    adding first string extensions

---
+++ NExtensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace NExtensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmpty(this string value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
+
+        public static bool IsNullOrWhiteSpace(this string value)
+        {
+            return string.IsNullOrWhiteSpace(value);
+        }
+    }
+}
