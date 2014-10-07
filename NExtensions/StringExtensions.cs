9547c43d5c553772e3f7ac724f51a527df34ee5d
halcharger <allen.firth@gmail.com>
2014-10-07 14:29:17 +0200

    adding Base64Encode and Decode string extensions

--- NExtensions/StringExtensions.cs
+++ NExtensions/StringExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace NExtensions
 {
@@ -218,6 +219,19 @@ namespace NExtensions
 
             return (int) Convert.ToDecimal(s);
         }
+
+        public static string Base64Encode(this string plainText)
+        {
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            return Convert.ToBase64String(plainTextBytes).Replace("/", "-");
+        }
+
+        public static string Base64Decode(this string base64EncodedData)
+        {
+            var base64EncodedBytes = Convert.FromBase64String(base64EncodedData.Replace("-", "/"));
+            return Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+
     }
 
     [Flags]
